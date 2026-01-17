eeb39cf7a065d882f333863e0492b5a5284c65fa
桥下红药 <ai121@foxmail.com>
2026-01-17 17:27:14 +0800

    update

--- examples/main.rs
+++ examples/main.rs
@@ -11,7 +11,7 @@ use std::sync::atomic::{AtomicUsize, Ordering};
 async fn main() -> Result<()> {
 
     if std::env::var("RUST_LOG").is_err() {
-        std::env::set_var("RUST_LOG", "info");
+        unsafe { std::env::set_var("RUST_LOG", "info"); }
     }
 
     tracing_subscriber::fmt()
